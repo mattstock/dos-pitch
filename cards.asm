44ac94672c700d67b3472c143d35e39ab1e10077
Matt Stock <stock@csgeeks.org>
2022-08-27 10:01:01 -0400

    better hex printing code implemented

--- cards.asm
+++ cards.asm
@@ -9,75 +9,75 @@
 
     .DATA
 
-    Suits DB 03,04,05,06
-    Cards DB 'A234567890JQK'
-    Deck DB 104 DUP(?)
+    Cards DB 'A',03,'2',03,'3',03,'4',03,'5',03,'6',03,'7',03
+         DB '8',03,'9',03,'0',03,'J',03,'Q',03,'K',03 
+         DB 'A',04,'2',04,'3',04,'4',04,'5',04,'6',04,'7',04
+         DB '8',04,'9',04,'0',04,'J',04,'Q',04,'K',04 
+         DB 'A',05,'2',05,'3',05,'4',05,'5',05,'6',05,'7',05
+         DB '8',05,'9',05,'0',05,'J',05,'Q',05,'K',05 
+         DB 'A',06,'2',06,'3',06,'4',06,'5',06,'6',06,'7',06
+         DB '8',06,'9',06,'0',06,'J',06,'Q',06,'K',06 
+    Deck DB 52 DUP(?)
+    Msg DB 'This is a test string$'
     
     .CODE
     EXTRN Rand:PROC
     EXTRN RandInit:PROC
     EXTRN PrintHex:PROC
+    EXTRN PrintHexByte:PROC
     
-PROC ProgramStart
+ProgramStart:   
     mov ax, @data
     mov ds, ax
     mov es, ax
-    call RandInit
-    call GenerateCards
-    mov si,OFFSET Deck
-    mov cx, 52
-s2:
-    lodsw
-    mov bx, ax
-    mov dl, bl
-    mov ah, 2
-    int 21h
-    mov dl, bh
-    int 21h
-    mov dl, ','
-    int 21h
-    loop s2
+    
+;    call RandInit
+;    call ShuffleDeck
+    mov al, 0e4h
+    call PrintHexByte
+    mov al, 56h
+    call PrintHexByte
+    mov ax, 4edah
+    call PrintHex
     mov ah, 4ch
     mov al, 0
     int 21h
-ENDP ProgramStart
 
-PROC GenerateCards
-    cld
-    mov di,OFFSET Deck
-    mov bx,OFFSET Suits
-    mov dx, 4
-s1:     
-    mov ah, [bx] ; load suit symbol
-    mov cx, 13
-    mov si,OFFSET Cards
-cardloop:
-    lodsb ; pull in card number to AL
-    stosw ; write AX with card and suit
-    loop cardloop
-    inc bx
-    dec dx
-    jnz s1
-    
+
+    PROC PrintDeck
+    mov si,OFFSET Deck
+    mov cx, 52
+s2:
+    lodsb
+    call PrintHex
+    loop s2
     ret
-    ENDP GenerateCards
+    ENDP PrintDeck
 
-    ; Pick a random card
-PROC RandCard
+    
+
+
+
+
+    
+    ; ShuffleDeck generates a random deck from the cards
+    PROC ShuffleDeck
+    mov si,OFFSET Deck
+    mov cx, 52
+    mov dl, 52
+s1:
     call Rand
-    mov ah, 0
-    mov cl, 13
-    div cl              ; ah is a card index
+    mov ah,0
+    div dl
+    push ax
+    call PrintHexByte
+    pop ax
+    xor bx,bx
     mov bl, ah
-    mov bh, 0
-    mov dh, [Cards+bx]
-    call Rand
-    shr ax, 14          ; 4 suits so only use top 2 bits
-    mov bx, ax
-    mov dl, [Suits+bx]
-    mov ax, dx
+    mov al, [si+bx]
+    call PrintHex
+    mov [si+bx], cl
+    loop s1
     ret
-    ENDP RandCard
-    
-    
+    ENDP ShuffleDeck
 END
--- misc.asm
+++ misc.asm
@@ -1,44 +1,59 @@
     DOSSEG
     .MODEL small
 
+    .DATA
 
+    HexDigits DB '0123456789ABCDEF'
+    Result DB 3 DUP(?)
+    
     .CODE
 
+    ; al = byte to print
+    ;
+    ; destroys di, ax, bx, cx, dx
+    PUBLIC PrintHexByte
+    PROC PrintHexByte
+
+    mov BYTE PTR [Result+2], '$'  ; terminate string
+    mov di, OFFSET Result+1
+    std
+    xor bh, bh
+    mov cl, 4
+
+    mov bl, al
+    and bl, 0fh
+    mov ah, [HexDigits+bx]
+    mov bl, al
+    xchg ah, al
+    stosb                       ; write the value to result string
+
+    shr bl, cl                  ; shift the 2nd nibble
+    mov al, [HexDigits+bx]      ; index to character
+    stosb                       ; write the value to result string
+    mov dx, OFFSET Result
+    mov ah, 9
+    int 21h
+    ret
+    ENDP PrintHexByte
+    
 ;;; AX is the hex number to be printed
     PUBLIC PrintHex
-    
-    PrintHex PROC NEAR
+    PROC PrintHex
+    push ax
+    push bx
     push cx
     push dx
-    mov cl, 16
-loop2:
-    sub cl, 4
-    mov dx, ax
-    shr dx, cl
-    and dl, 0fh
+    push di
     push ax
-    mov ah, 2
-    cmp dl, 9h
-    jg alpha
-    add dl, '0'
-    jmp p1
-alpha:
-    sub dl, 10
-    add dl, 'A'
-p1:     
-    int 21h
+    xchg ah,al
+    call PrintHexByte
     pop ax
-    cmp cl, 0
-    jnz loop2
-    push ax
-    mov ah, 2
-    mov dl, 0dh
-    int 21h
-    mov dl, 0ah
-    int 21h
-    pop ax
+    call PrintHexByte
+    pop di
     pop dx
     pop cx
+    pop bx
+    pop ax
     ret
     ENDP PrintHex
 END
